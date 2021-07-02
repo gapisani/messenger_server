33d168302c343e0dd288815900aa74b41f052820
gapisani <53335706+gapisani@users.noreply.github.com>
2021-07-02 12:27:08 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
-import json, bcrypt, os, socket
+import json, bcrypt, os, socket, sys
 from config import config
 from chat_list import chat_list
 from Log import Log
 
 # TODO: Создать личные сообщения
+
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
 
 class Server:
     def __init__(self, ip, port):
@@ -11,7 +13,12 @@ class Server:
         self.ip = ip
         self.port = port
         self.clients = {}
-        Log("Starting server...").log()
+        if(not(os.path.isdir(APP_PATH+"/save_data"))):
+            os.mkdir(APP_PATH+"/save_data")
+        if(not(os.path.isfile(APP_PATH+"/save_data/passwords.json"))):
+            open(APP_PATH+"/save_data/passwords.json", "w").write("{}")
+        if(not(os.path.isfile(APP_PATH+"/save_data/message_history.json"))):
+            open(APP_PATH+"/save_data/message_history.json", "w").write("[]")
 
     def resend_message(self, data_json, addres): # Пересыл сообщений от 1 пользователя для всех
         nickname = data_json["name"] # Получения имя от пользователя
@@ -20,6 +27,7 @@ class Server:
             if(self.chat_check(data_json["chat"], nickname, addres, data_json["chat_key"])):
                 chat = data_json["chat"]
                 if(data_json["join"]): # Если это сообщение о входе
+                    self.sock.sendto(json.dumps(self.load_messages_from_history()).encode("utf-8"), addres)
                     Log(f"{nickname}({addres[0]}:{addres[1]}) was connected!").successfully() # Выводим сообщение о подключении
                     result = f"{nickname} connected!" # Состовляем сообщение о подключении
                     nickname = "SERVER"
@@ -34,16 +42,20 @@ class Server:
                 else: # Иначе(Если это не сообщение о подключении и не сообщение о отключении)
                     msg = data_json["message"] # Получаем сообщение
                     result = f"{msg}" # Составяем сообщение
+                final_message = [{"nickname": nickname, "message": result, "chat":chat}]
                 for client in self.clients[chat]: # Перебираем клиентов
-                    self.sock.sendto(json.dumps([{"nickname": nickname, "message": result, "chat":chat}]).encode('utf-8'), client) #Отсылаем сообщения клиентам
+                    self.sock.sendto(json.dumps(final_message).encode('utf-8'), client) #Отсылаем сообщения клиентам
                 Log(f"[{nickname}]: {result}", chat).chat() # Выводим результат в консоль
+                self.write_message_to_history(final_message)
             else:
-                self.sock.sendto(json.dumps([{"message":"Server hasn't this chat.", "chat":data_json["chat"]}]).encode("utf-8"), addres)
+                self.sock.sendto(json.dumps([{"message":"Server hasn't this chat.", "chat":data_json["chat"], "nickname": "SERVER"}]).encode("utf-8"), addres)
         else: #Если пароль не правильный
-            self.sock.sendto(json.dumps([{"message":"Wrong password!", "chat":"#main"}]).encode("utf-8"), addres) # Отсылаем Клиенту сообщение о неправильном пароле
+            self.sock.sendto(json.dumps([{"message":"Wrong password!", "chat":"#main", "nickname":"SERVER"}]).encode("utf-8"), addres) # Отсылаем Клиенту сообщение о неправильном пароле
+        
 
 
     def start(self): # Запуск сервера
+        Log("Starting server...").log()
         try:
             Log("Binding IP...").log()
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Выделяем себе IP
@@ -53,30 +65,26 @@ class Server:
             while True: # Всегда
                 data, addres = self.sock.recvfrom(1024) # Читаем сообщения от клиентов
                 self.resend_message(json.loads(data.decode("utf-8")), addres) # Вызываем функцию пересыла сообщений
+                self.__str__ = f"Server on {self.ip}:{self.port}, {len(self.clients['#main'])} online." # Обновление статуса сервера
         except Exception as e: # Если произошла ошибка
-            print(self.clients)
-            for client in self.clients["#main"]: self.sock.sendto(json.dumps([{"message":"Server error", "chat":"#main"}]).encode("utf-8"), client) # Отсылаем сообщения о выходе клиентам
-            self.sock.close() # Закрываем сокет
-            raise(e) # Вызываем ошибку
+            print("Error:", e)
+            self.stop()
 
     def password_check(self, user, password): # Функция проверки пароля
-        if(not(os.path.isfile("Passwords.json"))): open("Passwords.json", "w").write("{}") # Если базы данных нет то создаем
-        with open("Passwords.json", "r") as get_db: # Читаем базу данных
+        with open(APP_PATH+"/save_data/passwords.json", "r") as get_db: # Читаем базу данных
             db = json.load(get_db) # Загружаем базу данных
         if(user in db): # Если пользователь в базе данных
             if(bcrypt.checkpw(password.encode("utf-8"), db[user].encode("utf-8"))): return(True) # Проверяем пароль
             else: return(False)
         else: # Если пользователя нет в базе данных
             db[user] = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8") # Хешируем его пароль
-            with open("Passwords.json", "w") as write_db: # Открываем базу данных для записи
+            with open(APP_PATH+"/save_data/passwords.json", "w") as write_db: # Открываем базу данных для записи
                 json.dump(db, write_db, sort_keys=True, indent=4) # Записываем туда обновленую версию
             return(True)
 
     def chat_check(self, chat, user, addres, key=None): # Функция проверки чата
-        print(self.clients)
         if(chat in chat_list): # Если такой чат вообще есть на сервере
             if(chat not in self.clients):
-                print("Chat is not in self.clients")
                 self.clients[chat] = {}
             if(chat_list[chat]["join_key"]): # Если у чата есть ключ для входа
                 if(key == chat_list[chat]["join_key"]): # Если он совпадает с тем что есть у пользователя
@@ -90,4 +98,28 @@ class Server:
         else: # Если чата нет на сервере
             return(False) # возвращаем False
 
-Server(config["ip"], config["port"]).start()
+    def command(self):
+        pass
+
+    def write_message_to_history(self, message):
+        history_file = APP_PATH+"/save_data/message_history.json"
+
+        db = json.load(open(history_file, "r"))
+        db += message
+        json.dump(db, open(history_file, "w"))
+
+    def load_messages_from_history(self):
+        history_file = APP_PATH+"/save_data/message_history.json"
+        db = json.load(open(history_file, "r"))
+        return(db)
+
+    def stop(self):
+        for client in self.clients["#main"]: self.sock.sendto(json.dumps([{"message":"Server error", "chat":"#main", "nickname":"SERVER"}]).encode("utf-8"), client) # Отсылаем сообщения о выходе клиентам
+        self.sock.close() # Закрываем сокет
+        sys.exit()
+
+s = Server(config["ip"], config["port"])
+try:
+    s.start()
+except KeyboardInterrupt:
+    s.stop()
